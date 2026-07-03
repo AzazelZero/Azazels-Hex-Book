918b556e4dce1af16dacd5117005faed32ba4f92
azalea <username200002@gmail.com>
2026-07-03 19:33:26 -0400

    Added Patterns.java

--- gitadd.sh
+++ gitadd.sh
@@ -1,2 +1,2 @@
+cp AzazelsHexes/common/src/main/java/net/digideeps/azazelshexes/registry/Patterns.java .
 git add *
-git add -f AzazelsHexes/common/src/main/java/net/digideeps/azazelshexes/registry/Patterns.java
