22cc8699e945b2d7ab6b9882df4a82db68dcc6fb
azalea <username200002@gmail.com>
2026-07-06 19:51:47 -0400

    resources

--- gitadd.sh
+++ gitadd.sh
@@ -1,7 +1,4 @@
 cp AzazelsHexes/common/src/main/java/net/digideeps/azazelshexes/registry/Patterns.java .
-git add -f .github
-git add -f .hexdoc
-git add -f .env
-git add -f .python-version
-git add -f .copier-answers.hexdoc-hexcasting.yml
+git add -f AzazelsHexes/common/src/main/resources/*
 git add *
+
