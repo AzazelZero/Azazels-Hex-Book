09727388247a924a84348c5bfebe302f3e838c7f
azalea <username200002@gmail.com>
2026-07-06 20:17:30 -0400

    more resources

--- gitadd.sh
+++ gitadd.sh
@@ -1,4 +1,6 @@
 cp AzazelsHexes/common/src/main/java/net/digideeps/azazelshexes/registry/Patterns.java .
-git add -f AzazelsHexes/common/src/main/resources/*
+cp AzazelsHexes/common/src/main/resources/commonResources
+cp AzazelsHexes/forge/src/main/resources/forgeResources
+cp AzazelsHexes/c/src/main/resources/fabricResources
 git add *
 
